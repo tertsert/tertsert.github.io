f3e95d189ab05eae4a082e7449a0f22da1721abb
tertsert <oscarismylizard@gmail.com>
2022-04-26 12:43:49 -0500

    Editing changes

--- config.js
+++ config.js
@@ -3,25 +3,24 @@ var config = {
     accessToken: 'pk.eyJ1IjoibmRlbGtzIiwiYSI6ImNremhtazY1ajFhc3IzMW9iam5haDJubWUifQ.4o2Mrbi4Vch1fv77PttorA',
     showMarkers: false,
     markerColor: '#3FB1CE',
-    theme: 'light',
+    theme: 'dark',
     use3dTerrain: false,
-    title: 'Heirs Property Map',
-    subtitle: 'Seabreeze Bop City',
-    byline: 'By Naomi Delkamiller and Michael Ryan',
+    title: '',
+    subtitle: '',
+    byline: '',
     footer: 'Source: source citations, etc.',
     chapters: [
         {
-            id: 'Chapter 1: The Barrel',
-            alignment: 'right',
+            id: 'Intro slide',
+            alignment: 'left',
             hidden: false,
-            title: 'Seabreeze Myrtle Sound',
-            image: 'https://imgur.com/C3q0u4m.jpg',
-            description: "Seabreeze's origins were established on 99 acres of undeveloped land purchased before the end of the civil war by formerly enslaved Alexander and Charity Freeman. Over the hundred years that followed, the area developed into a thriving beach resort and one of the only places open to African Americans along the east coast during Jim Crow",
+            title: 'Understanding Heirs property: A Seabreeze Bop City Project',
+            description: "By Naomi Delkamiller and Michael Ryan",
             location: {
-                center: [-77.88386, 34.06508], 
-				zoom: 14.99, 
-				pitch: 60.00, 
-				bearing: 0.00 
+                center: [-77.89556, 34.06317],
+                zoom: 15.22,
+                pitch: 53.50,
+                bearing: -10.40
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
@@ -50,17 +49,17 @@ var config = {
 	    ]
         },
         {
-            id: 'Chapter 1 sub 1: The Barrel',
+            id: 'Intro to Heirs property',
             alignment: 'right',
             hidden: false,
             title: 'Seabreeze Myrtle Sound',
             image: 'tertsert.github.io/images/Freeman family article.jpeg',
             description: "After segregation ended, and after a sequence of human- made and natural coastal disasters, Seabreeze faded as a resort community. Today, there are few physical traces left to recall the vibrancy of its heyday. Many extended Freeman family members are still in residence, though much of the property in the neighborhood is undevelopable and under threat as heirs property.",
             location: {
-               	center: [-77.88386, 34.06508], 
-				zoom: 14.99, 
-				pitch: 60.00, 
-				bearing: 0.00 
+                center: [-77.88558, 34.06285],
+                zoom: 14.66,
+                pitch: 0.50,
+                bearing: 0.00 
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
@@ -69,17 +68,17 @@ var config = {
             onChapterExit: []
         },
         {
-            id: 'Chapter 2: Wake County',
+            id: 'Explanation of Seabreeze',
             alignment: 'right',
             hidden: false,
             title: 'Wake County',
             image: 'tertsert.github.io/images/Townships.png',
             description: 'Filler text',
             location: {
-               	center: [-78.38189, 35.78975],
-				zoom: 9.38,
-				pitch: 0.00,
-				bearing: 0.00
+                center: [-77.88558, 34.06285],
+                zoom: 14.66,
+                pitch: 0.50,
+                bearing: 0.00 
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
@@ -123,8 +122,19 @@ var config = {
             mapAnimation: 'flyTo',
             rotateAnimation: false,
             callback: '',
-            onChapterEnter: [],
-            onChapterExit: []
+            onChapterEnter: [
+                {
+            		layer: 'ndelks-4xm30cw1',
+                 	opacity: 1
+                }
+            ],
+            
+            onChapterExit: [
+                {
+            		layer: 'ndelks-4xm30cw1',
+                 	opacity: 0
+                }
+            ]
         },
         {
             id: 'Chapter 3: American South',
