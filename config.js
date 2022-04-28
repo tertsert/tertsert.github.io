c9d4d88fcd1d4717de44d883643c96fefe5357d9
tertsert <oscarismylizard@gmail.com>
2022-04-27 19:11:37 -0500

    Getting stuff done!

--- config.js
+++ config.js
@@ -8,19 +8,19 @@ var config = {
     title: '',
     subtitle: '',
     byline: '',
-    footer: 'Source: source citations, etc.',
+    footer: 'Bartels, C. (2012). Identifying and addressing heirs property: a case study of Wake County, North Carolina. https://doi.org/10.17615/86ms-ry09',
     chapters: [
         {
             id: 'Intro slide',
             alignment: 'left',
             hidden: false,
-            title: 'Understanding Heirs property: A Seabreeze Bop City Project',
+            title: "Understanding Heirs' property: A Seabreeze Bop City Project",
             description: "By Naomi Delkamiller and Michael Ryan",
             location: {
-                center: [-77.89556, 34.06317],
-                zoom: 15.22,
-                pitch: 53.50,
-                bearing: -10.40
+                center: [-77.89126, 34.06249],
+                zoom: 15.00,
+                pitch: 54.50,
+                bearing: -32.80
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
@@ -49,17 +49,16 @@ var config = {
 	    ]
         },
         {
-            id: 'Intro to Heirs property',
+            id: "Intro to Heirs' property",
             alignment: 'right',
             hidden: false,
-            title: 'Seabreeze Myrtle Sound',
-            image: 'tertsert.github.io/images/Freeman family article.jpeg',
-            description: "After segregation ended, and after a sequence of human- made and natural coastal disasters, Seabreeze faded as a resort community. Today, there are few physical traces left to recall the vibrancy of its heyday. Many extended Freeman family members are still in residence, though much of the property in the neighborhood is undevelopable and under threat as heirs property.",
+            title: "What is Heirs' property?",
+            description: "African American families throughout the United States owned 15 million acres of property in 1910. By 1997, that number had dropped to 2.4 million acres (Federal of Southern Cooperatives Land Assistance Fund). One of the primary factors of this land reduction was heirs property- an unstable form of property ownership that is used in cases without a will or deed. Heirs property is a serious legal issue because the lack of documentation makes it difficult to determine where the property is and how much exists (Bartles).",
             location: {
-                center: [-77.88558, 34.06285],
-                zoom: 14.66,
-                pitch: 0.50,
-                bearing: 0.00 
+                center: [-77.88816, 34.06255],
+                zoom: 15.58,
+                pitch: 0.00,
+                bearing: 7.20
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
@@ -71,14 +70,13 @@ var config = {
             id: 'Explanation of Seabreeze',
             alignment: 'right',
             hidden: false,
-            title: 'Wake County',
-            image: 'tertsert.github.io/images/Townships.png',
-            description: 'Filler text',
+            title: 'Who is the Freeman family?',
+            description: "The land south of Myrtle Sound, North Carolina is known as Seabreeze. It's origins were established on 99 acres of undeveloped land purchased before the end of the Civil War by formerly enslaved Alexander and Charity Freeman. Over the hundred years that followed, the area developed into a thriving beach resort and one of the only places open to African Americans along the east coast during Jim Crow (Seabreeze Bop City). The land has since been divided and passed throughout the family for generations.",
             location: {
-                center: [-77.88558, 34.06285],
-                zoom: 14.66,
-                pitch: 0.50,
-                bearing: 0.00 
+                center: [-77.88970, 34.06310],
+                zoom: 17.22,
+                pitch: 0.00,
+                bearing: 7.20   
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
@@ -114,10 +112,10 @@ var config = {
             image: 'tertsert.github.io/images/Townships.png',
             description: 'Filler text',
             location: {
-               	center: [-78.38189, 35.78975],
-				zoom: 9.38,
-				pitch: 0.00,
-				bearing: 0.00
+                center: [-77.88983, 34.06317],
+                zoom: 17.42,
+                pitch: 0.00,
+                bearing: 7.20
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
